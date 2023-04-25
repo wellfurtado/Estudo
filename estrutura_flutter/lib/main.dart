b357da4b7a0e025bccfb65b35844b7929d5ec919
Wellington Damasceno <wellfurtado@gmail.com>
2023-04-25 10:43:36 -0300

    Entendendo estrutura flutter

--- estrutura_flutter/lib/main.dart
+++ estrutura_flutter/lib/main.dart
@@ -1,20 +1,21 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(
+      MinhaApp()
+  );
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
+class MinhaApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        appBar: AppBar(
+          title: const Text('Wellington App'),
         ),
-      ),
+        body: const Text('Teste'),
+      )
     );
   }
 }
